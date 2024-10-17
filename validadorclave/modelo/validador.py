b45e1af88ff12898dfac5300b31d6a7fcdc65db8
Andres Ortega <aortega473@soyudemedellin.edu.co>
2024-10-17 12:14:45 -0500

    Finish class ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -7,3 +7,17 @@ class ReglaValidacion(ABC):
     @abstractmethod
     def es_valida(self, clave):
         pass
+
+    def _contiene_mayuscula(self, clave):
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave):
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave):
+        return any(c.isdigit() for c in clave)
+
+    def _validar_longitud(self, clave):
+        return len(clave) >= self.longitud_minima
+
+
