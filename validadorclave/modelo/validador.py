a0d2ce6e99d39bc01a2c8a17fc0346b447a24835
Andres Ortega <aortega473@soyudemedellin.edu.co>
2024-10-17 12:10:29 -0500

    Add class ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,9 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_minima=8):
+        self.longitud_minima = longitud_minima
+
+    @abstractmethod
+    def es_valida(self, clave):
+        pass
